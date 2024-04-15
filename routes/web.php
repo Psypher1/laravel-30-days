b414d9a1d916d5290d7f2ebff165670c22a8ff27
dante <jamesmidzi@gmail.com>
2024-04-15 20:29:40 +0200

    lesson: worked through course material

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// declare route that listens for a get request to the home page
+// in response , if visited, trigger a funciton that returns a view called welcome
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// can return string, or array
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+
